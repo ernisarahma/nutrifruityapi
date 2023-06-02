03f0c7c178683e2002434b53d309ebb0fcdbdb3a
ernisarahma <126392609+ernisarahma@users.noreply.github.com>
2023-06-02 18:16:22 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 // Konfigurasi koneksi database
 const config = mysql.createPool({
-    host: '34.128.107.121',
-    user: 'nutrifruity',
-    password: 'nutrifruity',
+    host: '34.126.92.129',
+    user: 'nutrifruity1',
+    password: 'nutrifruity1',
     database: 'nutrifruity',
 });
 
